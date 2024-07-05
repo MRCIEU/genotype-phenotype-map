636c592085671879362ef27ece9799e3795fb6fd
Andrew Elmore <andrew.elmore@bristol.ac.uk>
2024-07-05 16:41:04 +0100

    Adding additional output to compile results

--- common_cis_variants/compile_results.R
+++ common_cis_variants/compile_results.R
@@ -2,20 +2,28 @@ source('constants.R')
 
 POSTERIOR_PROB_THRESHOLD <- 0.8
 
-parser <- argparser::arg_parser('Compile results')
-parser <- argparser::add_argument(parser, "--studies_to_process", help = "Studies to process", type = "character")
-parser <- argparser::add_argument(parser, "--studies_processed", help = "Current state of processed studies", type = "character")
-parser <- argparser::add_argument(parser, "--coloc_result_files", help = "Coloc result files", type = "character", nargs = Inf)
-parser <- argparser::add_argument(parser, "--compiled_results_file", help = "Compiled result file to save", type = "character")
+parser <- argparser::arg_parser('Compile results from pipeline')
+#INPUT
+parser <- argparser::add_argument(parser, '--studies_to_process', help = 'Studies to process', type = 'character')
+parser <- argparser::add_argument(parser, '--studies_processed', help = 'Current state of processed studies', type = 'character')
+parser <- argparser::add_argument(parser, '--coloc_input_files', help = 'All files from the perform coloc pipeline step', type = 'character', nargs = Inf)
+#OUTPUT
+parser <- argparser::add_argument(parser, '--all_study_regions_file', help = 'Compiled result file to save', type = 'character')
+parser <- argparser::add_argument(parser, '--coloc_results_file', help = 'Compiled result file to save', type = 'character')
+parser <- argparser::add_argument(parser, '--compiled_results_metadata_file', help = 'Compiled result metadata file to save', type = 'character')
+
 args <- argparser::parse_args(parser)
 
 main <- function(args) {
   all_studies_processed <- update_processed_study_metadata(args$studies_to_process, args$studies_processed)
+  coloc_results <- compile_coloc_results(args$coloc_input_files, all_studies_processed)
 
-  coloc_result_files <- split_string_into_vector(args$coloc_result_files)
-  compiled_results <- compile_coloc_results(coloc_result_files, all_studies_processed)
+  all_study_regions <- compile_entire_list_of_extracted_study_regions()
+  results_metadata <- aggregate_pipeline_metadata()
 
-  vroom::vroom_write(compiled_results, args$compiled_results_file)
+  vroom::vroom_write(coloc_results, args$coloc_results_file)
+  vroom::vroom_write(all_study_regions, args$all_study_regions_file)
+  vroom::vroom_write(results_metadata, args$compiled_results_metadata_file)
 }
 
 update_processed_study_metadata <- function(studies_to_process_file, studies_processed_file) {
@@ -29,14 +37,50 @@ update_processed_study_metadata <- function(studies_to_process_file, studies_pro
   vroom::vroom_write(studies_processed, studies_processed_file)
 }
 
-compile_coloc_results <- function(coloc_result_files, studies_processed) {
-  significant_results <- lapply(coloc_result_files, function(result_file) {
-    if (!file.exists(result_file)) return()
-    result <- vroom::vroom(result_file, delim='\t', show_col_types=F)
-    #TODO: remove the last %in% names check once the bug has been fixed
-    #if (is.null(result) || nrow(result) == 0 || !'posterior_prob' %in% names(result)) return ()
+compile_entire_list_of_extracted_study_regions <- function() {
+  ld_regions <- vroom::vroom('data/ld_regions.tsv', show_col_types = F)
+  ld_block_dirs <- paste0(ld_block_data_dir, ld_regions$pop, '/', ld_regions$chr, '/', ld_regions$start, '_', ld_regions$stop, '/')
 
-    significant_result <- dplyr::filter(result, !is.na(posterior_prob) & posterior_prob >= POSTERIOR_PROB_THRESHOLD)
+  all_finemapped_studies <- lapply(ld_block_dirs, function(ld_block_dir) {
+    finemapped_studies <- vroom::vroom(paste0(ld_block_dir, 'finemapped_studies.tsv'), show_col_types = F)
+  }) |>
+    dplyr::bind_rows() |>
+    dplyr::select(study, file, chr, bp)
+
+  #TODO: populate both 'known' (from coloc results) and 'suspected 'genes associated here?
+  return(all_finemapped_studies)
+}
+
+aggregate_pipeline_metadata <- function() {
+  ld_regions <- vroom::vroom('data/ld_regions.tsv', show_col_types = F)
+  ld_block_dirs <- paste0(ld_block_data_dir, ld_regions$pop, '/', ld_regions$chr, '/', ld_regions$start, '_', ld_regions$stop, '/')
+
+  lapply(ld_block_dirs, function(ld_block_dir) {
+    if (!file.exists(paste0(ld_block_dir, 'extracted_studies.tsv'))) {
+      return()
+    }
+    extracted_studies <- vroom::vroom(paste0(ld_block_dir, 'extracted_studies.tsv'), show_col_types = F)
+    imputed_studies <- vroom::vroom(paste0(ld_block_dir, 'imputed_studies.tsv'), show_col_types = F)
+    finemapped_studies <- vroom::vroom(paste0(ld_block_dir, 'finemapped_studies.tsv'), show_col_types = F)
+    deduplicated_finemapped_studies <- finemapped_studies[!duplicated(finemapped_studies$study),]
+
+    return(data.frame(ld_region = sub(, ld_block_dir),
+                      extracted_studies=nrow(extracted_studies),
+                      studies_imputed=nrow(imputed_studies),
+                      mean_snps_imputed=mean(imputed_studies$rows_imputed),
+                      number_finemapped=nrow(finemapped_studies),
+                      finemap_success=sum(deduplicated_finemapped_studies$message == 'success'),
+                      finemap_faile=sum(deduplicated_finemapped_studies$message == 'failed'),
+                      finemap_no_need=sum(deduplicated_finemapped_studies$message == 'less_than_2_cs')
+    ))
+  }) |> dplyr::bind_rows()
+}
+
+compile_coloc_results <- function(coloc_input_files, studies_processed) {
+  significant_results <- lapply(coloc_input_files, function(coloc_file) {
+    coloc <- vroom::vroom(coloc_file)
+    if (is.null(coloc)) return ()
+    significant_result <- dplyr::filter(coloc, !is.na(posterior_prob) & posterior_prob >= POSTERIOR_PROB_THRESHOLD)
     if (nrow(significant_result) > 0) return(significant_result)
   }) |> dplyr::bind_rows()
 
@@ -53,7 +97,8 @@ compile_coloc_results <- function(coloc_result_files, studies_processed) {
       tidyr::separate(col = 'trait_b', into = c('study_b', 'ancestry_b', 'chr_b', 'bp_b', 'finemap_version_b'), sep='_', remove = F) |>
       dplyr::mutate(posterior_prob = as.numeric(result['posterior_prob']),
                     candidate_snp = as.character(result['candidate_snp']),
-                    posterior_explained_by_snp = as.numeric(result['posterior_explained_by_snp'])
+                    posterior_explained_by_snp = as.numeric(result['posterior_explained_by_snp']),
+                    cis_trans = NA
       )
     paired_results$data_type_a <- studies_processed$data_type[match(paired_results$study_a, studies_processed$study_name)]
     paired_results$data_type_b <- studies_processed$data_type[match(paired_results$study_b, studies_processed$study_name)]

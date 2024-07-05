64fc45c93b5f557b4a9db1f0f9ffac4384bcb74b
Andrew Elmore <andrew.elmore@bristol.ac.uk>
2024-07-05 18:37:45 +0100

    Adding biomart to docker image

--- docker/requirements.R
+++ docker/requirements.R
@@ -1,9 +1,10 @@
-cran_install <- c("susieR", "Rfast")
-install.packages(cran_install, repos = "http://cran.us.r-project.org")
+cran_install <- c('susieR', 'Rfast', 'BiocManager')
+install.packages(cran_install, repos = 'http://cran.us.r-project.org')
 
-devtools::install_github("ZikunY/CARMA")
-remotes::install_version("RcppEigen", version = "0.3.3.9.3")
-devtools::install_github("jrs95/hyprcoloc", upgrade = "never")
+devtools::install_github('ZikunY/CARMA')
+remotes::install_version('RcppEigen', version = '0.3.3.9.3')
+devtools::install_github('jrs95/hyprcoloc', upgrade = 'never')
 
-#biocmanager_install <- c("Homo.sapiens", "GenomicRanges")
-#BiocManager::install(biocmanager_install)
+biocmanager_install <- c('biomaRt')
+BiocManager::install(biocmanager_install)
+

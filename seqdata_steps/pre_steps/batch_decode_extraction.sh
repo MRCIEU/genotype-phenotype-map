2436fb1addcc9a0cbe54739aa3b1fef98e5a03ec
Andrew Elmore <andrew.elmore@bristol.ac.uk>
2024-08-23 10:37:21 +0100

    making small changes to the decode extraction scripts (#26)

--- seqdata_steps/pre_steps/batch_decode_extraction.sh
+++ seqdata_steps/pre_steps/batch_decode_extraction.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 LIST=$1
-NUM_PARALLEL=250
+NUM_PARALLEL=30
 
 DIR_NAME=/local-scratch/data/ukb-seq/downloads/halldorexwas/decode_data
 
@@ -9,6 +9,7 @@ count=1
 for FILE in $(cat ${LIST}); do
   BASENAME=$(basename ${FILE} .txt.gz)
   LOG_FILE=$DIR_NAME/$BASENAME.log
+  echo "processing $FILE"
   ./01_pullfromdecode.sh $FILE &> $LOG_FILE &
 
   count=$(( count + 1 ))

b47e3b2093a8b97dc38fbc837b5d2a8482ff9f24
alhbiostat <aimee.hanson@bristol.ac.uk>
2024-09-24 11:23:31 +0100

    hail export

--- seqdata_steps/pre_steps/02_pullfromgenebass.py
+++ seqdata_steps/pre_steps/02_pullfromgenebass.py
@@ -6,6 +6,8 @@ import pandas as pd
 import hail as hl
 import os
 
+os.environ['PYSPARK_SUBMIT_ARGS'] = '--driver-memory 10g --executor-memory 10g pyspark-shell'
+
 DATA_DIR = "/local-scratch/data/genebass/"
 SEQDATA_DIR = "/local-scratch/data/ukb-seq/"
 
@@ -53,9 +55,10 @@ for study in range(1, n_studies + 1):
         (((study_stats.AF <= max_maf) & (study_stats.AC >= min_mac)) |
             ((1 - study_stats.AF <= max_maf) & (study_stats.call_stats.AN - study_stats.call_stats.AC >= min_mac))))
 
+    study_stats_rare = study_stats_rare.drop('saige_version','description_more','category')
+
     # Write out rare summary statistics
-    rare_df = study_stats_rare.to_pandas()
-    rare_df.to_csv(os.path.join(SEQDATA_DIR, "downloads/genebass/rare", outname_rare), sep = '\t', index = False)
+    study_stats_rare.export(os.path.join(SEQDATA_DIR, "downloads/genebass/rare", outname_rare))
 
     # Filter common (p<0.1, MAF>0.01)
     study_stats_common = study_stats.filter(
@@ -63,6 +66,7 @@ for study in range(1, n_studies + 1):
         (study_stats.AF > max_maf) & 
         (study_stats.AF < 1 - max_maf))
 
+    study_stats_common = study_stats_common.drop('saige_version','description_more','category')
+
     # Write out common summary statistics
-    commonm_df = study_stats_common.to_pandas()
-    common_df.to_csv(os.path.join(SEQDATA_DIR, "downloads/genebass/common", outname_common), sep = '\t', index = False)
+    study_stats_common.export(os.path.join(SEQDATA_DIR, "downloads/genebass/common", outname_common))

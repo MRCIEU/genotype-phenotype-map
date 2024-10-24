41ef8754a70bc6c812ed70e7ab0dbd492b7a626e
Andrew Elmore <andrew.elmore@bristol.ac.uk>
2024-10-24 12:51:23 +0100

    Stripping quotes to make it work

--- seqdata_steps/pre_steps/parallel_pullfromgenebass.sh
+++ seqdata_steps/pre_steps/parallel_pullfromgenebass.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
+set -e
 
 keys='/local-scratch/data/ukb-seq/downloads/genebass/study_keys.txt'
 
 while read -r k1 k2 k3 k4 k5; do
-    python3 02_pullfromgenebass.py ${k1} ${k2} ${k3} ${k4} ${k5}
+    echo "$k1 $k2 $k3 $k4 $k5"
+    python3 02_pullfromgenebass.py --trait_type ${k1} --phenocode ${k2} --pheno_sex ${k3} --coding ${k4} --modifier ${k5}
     rm *.log
 done < ${keys}
